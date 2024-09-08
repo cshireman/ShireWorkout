607a697e5d766681c15db898ac38d7b1a3ce407e
Christopher Shireman <chris@shireman.net>
2024-09-08 16:45:24 -0700

    Initial Commit

---
+++ ShireWorkout/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShireWorkout
+//
+//  Created by Chris Shireman on 9/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShireWorkout/ShireWorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShireWorkoutApp.swift
+//  ShireWorkout
+//
+//  Created by Chris Shireman on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ShireWorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
